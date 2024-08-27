fa299fc78e39603f580901d067098bbb655ddd43
Md Istiak Ahmed <istinishat@gmail.com>
2024-08-27 22:18:02 +0600

    fix bugs

--- app/dua/layout.tsx
+++ app/dua/layout.tsx
@@ -1,4 +1,3 @@
-import SecondaryHeader from "@/components/navbar2/SecondaryHeader";
 import { categoryMap } from "@/utils/constants";
 
 export default function DuaLayout({
@@ -10,7 +9,6 @@ export default function DuaLayout({
 
   return (
     <section>
-      <SecondaryHeader subNavList={subNavList} />
       <div className="flex items-center justify-center w-full">
         <div className=" w-[95vw] md:w-[70vw] py-[20px]">{children}</div>
       </div>
--- app/zakat/layout.tsx
+++ app/zakat/layout.tsx
@@ -1,4 +1,3 @@
-import SecondaryHeader from "@/components/navbar2/SecondaryHeader";
 import { categoryMap } from "@/utils/constants";
 
 export default function ZakatLayout({
@@ -10,7 +9,6 @@ export default function ZakatLayout({
 
   return (
     <section>
-      <SecondaryHeader subNavList={subNavList} />
       <div className="flex items-center justify-center w-full">
         <div className=" w-[95vw] md:w-[70vw] py-[20px]">{children}</div>
       </div>
