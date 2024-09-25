fc8edb583415c7b75a2ec5f5a683ed8c49a0c261
Md Istiak Ahmed <istinishat@gmail.com>
2024-09-25 19:40:32 +0600

    Add metadata

--- app/salat/salat-time/[country]/page.tsx
+++ app/salat/salat-time/[country]/page.tsx
@@ -1,8 +1,36 @@
 import FiveQaqtSalatComponent from "@/components/FiveQaqtSalatComponent";
 import { capitalize } from "@/utils/converters";
 import { getPrayerTime } from "@/utils/getData";
+import { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
+
+export async function generateMetadata({
+  params,
+}: any): Promise<Metadata | undefined> {
+  const { country } = params;
+
+  return {
+    title: `Muslim Prayer Times in ${country}: Your Reliable Guide to Daily Salah | IslamicTablig`,
+    description: `Discover the exact times for Fajr, Dhuhr, Asr, Maghrib, and Isha in ${country}. Our user-friendly tool makes it easy to find daily prayer schedules`,
+    openGraph: {
+      title: `Muslim Prayer Times in ${country}: Your Reliable Guide to Daily Salah | IslamicTablig`,
+      description: `Discover the exact times for Fajr, Dhuhr, Asr, Maghrib, and Isha in ${country}. Our user-friendly tool makes it easy to find daily prayer schedules`,
+      type: "article",
+      locale: "en_US",
+      url: `https://www.islamictablig.com/salat/salat-time/${country}`,
+      siteName: "IslamicTablig",
+      images: [
+        {
+          url: "/share.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Mosque",
+        },
+      ],
+    },
+  };
+}
 
 const CountryPrayerTimePage = async ({ params }: any) => {
   const { country } = params;
@@ -13,21 +41,30 @@ const CountryPrayerTimePage = async ({ params }: any) => {
 
   return (
     <div>
+      <h1 className="text-xl text-center">
+        Muslim Prayer Times in {capitalize(country)}: Your Reliable Guide to
+        Daily Salah
+      </h1>
       <FiveQaqtSalatComponent
         cityPrayerTime={capitalCityPrayerTime}
         city={`${country} (${capitalCity})`}
       />
+      <p className="text-md text-center py-2">
+        Discover the exact times for Fajr, Dhuhr, Asr, Maghrib, and Isha in{" "}
+        {country}. Our user-friendly tool makes it easy to find daily prayer
+        schedules.
+      </p>
       <h1 className="text-xl font-bold text-center py-4">
         {capitalize(country)} Prayer Time By City
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
         {cities.map((c: any, index: any) => (
           <Link
             href={`/salat/salat-time/${country}/${c.toLowerCase()}`}
             key={index}
             className="p-[24px] border-2 flex item-center justify-center cursor-pointer"
           >
-            <span>{capitalize(c)}</span>
+            <span>{capitalize(c)} prayer time</span>
           </Link>
         ))}
       </div>
--- app/salat/salat-time/page.tsx
+++ app/salat/salat-time/page.tsx
@@ -1,7 +1,29 @@
 import { countries } from "@/utils/constants";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: `Prayer Times Worldwide: Accurate Schedules for Every Country | IslamicTablig`,
+  description: `Discover the exact prayer times for your location. Our user-friendly tool makes it easy to find daily prayer schedules worldwide.`,
+  openGraph: {
+    title: `Prayer Times Worldwide: Accurate Schedules for Every Country | IslamicTablig`,
+    description: `Discover the exact prayer times for your location. Our user-friendly tool makes it easy to find daily prayer schedules worldwide.`,
+    type: "article",
+    locale: "en_US",
+    url: `https://www.islamictablig.com/salat/salat-time`,
+    siteName: "IslamicTablig",
+    images: [
+      {
+        url: "/share.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Mosque",
+      },
+    ],
+  },
+};
 
 const SalatTimePage = () => {
   return (
@@ -9,6 +31,12 @@ const SalatTimePage = () => {
       <h1 className="text-2xl font-bold text-center py-4">
         Prayer Time By Country
       </h1>
+      <p className="text-lg text-center py-5">
+        Discover the exact prayer times for your location. Our user-friendly
+        tool makes it easy to find daily prayer schedules worldwide.Stay
+        connected to your faith with accurate prayer times. Our reliable service
+        provides precise schedules for Muslims around the globe.
+      </p>
       <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
         {countries.map((c, index) => (
           <Link
@@ -34,6 +62,26 @@ const SalatTimePage = () => {
           </Link>
         ))}
       </div>
+
+      <h2 className="text-xl text-center pt-5 font-bold">
+        Factors Affecting Prayer Times
+      </h2>
+      <p className="text-lg text-center py-5">
+        Prayer times vary significantly across different countries due to
+        geographical location, time zone differences, and cultural practices.
+        Muslims, for example, follow a specific schedule based on the position
+        of the sun. This means that prayer times can differ even between nearby
+        cities.
+      </p>
+      <h2 className="text-xl text-center pt-5 font-bold">
+        Finding Accurate Prayer Times
+      </h2>
+      <p className="text-lg text-center py-5">
+        Thanks to modern technology, it is easier than ever to find accurate
+        prayer times for any location. There are numerous online resources,
+        mobile apps, and dedicated websites that provide detailed schedules for
+        various Islamic prayers like Fajr, Dhuhr, Asr, Maghrib, and Isha.
+      </p>
     </div>
   );
 };
